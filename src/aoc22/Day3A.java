e952729bfea603ba6db43c78d7a59a6644d8765a
Mohamed Nainar <nainar.mb@gmail.com>
2023-12-02 20:10:56 +0530

    aoc22d3

---
+++ src/aoc22/Day3A.java
@@ -0,0 +1,34 @@
+package aoc22;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static utils.Utils.print;
+
+public class Day3A {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("C:/dev/advent-of-code/inputs/submit"));
+        int sum = 0;
+        while(sc.hasNextLine()){
+            char res = solve(sc.nextLine());
+            if(res <= 'Z')
+                sum += (res - 'A') + 27;
+            else
+                sum += (res - 'a') + 1;
+        }
+        print("%d", sum);
+    }
+    static char solve(String str){
+        int len = str.length();
+        boolean[] freq = new boolean[128];
+        for(int i = 0; i < len / 2; i++)
+            freq[str.charAt(i)] = true;
+
+        for(int i = len / 2; i < len; i++)
+            if(freq[str.charAt(i)])
+                return str.charAt(i);
+
+        return ' ';
+    }
+}
---
+++ src/aoc22/Day3B.java
@@ -0,0 +1,31 @@
+package aoc22;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static utils.Utils.print;
+
+public class Day3B {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("C:/dev/advent-of-code/inputs/submit"));
+        int sum = 0, GROUP_SIZE = 3;
+        while(sc.hasNextLine()){
+            int[] freq = new int[128];
+            for(int i = 0; i < GROUP_SIZE; i++)
+                for(char c : sc.nextLine().toCharArray())
+                    freq[c] |= 1 << i;
+            char badge = ' ';
+            for(int i = 0; i < 128 && badge == ' '; i++)
+                if(freq[i] == 7)
+                    badge = (char) i;
+            if(badge <= 'Z'){
+                sum += (badge - 'A') + 27;
+            }
+            else{
+                sum += (badge - 'a') + 1;
+            }
+        }
+        print("%d", sum);
+    }
+}
