bcf2fd913f512593e1c06cc0d0650b775b43a458
Mohamed Nainar <nainar.mb@gmail.com>
2023-12-04 20:47:16 +0530

    aoc23d4a streams sum

--- src/aoc23/Day4A.java
+++ src/aoc23/Day4A.java
@@ -3,7 +3,6 @@ package aoc23;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -13,14 +12,12 @@ public class Day4A {
     public static void main(String[] args) throws IOException {
         String path = "C:/dev/advent-of-code/inputs/t";
 //        Scanner sc = new Scanner(new File(path));
-        var arr = (ArrayList<String>) Files.readAllLines(Paths.get(path));
-        int ans = 0;
-        for (String s : arr) {
+        int ans = Files.readAllLines(Paths.get(path)).stream().mapToInt(s -> {
             var nums = s.split(":")[1].split("\\|");
             var set = new HashSet<>(Arrays.asList(nums[0].trim().split(" +")));
-            int m = (int) Arrays.stream(nums[1].trim().split(" +")).filter(set::contains).count();
-            ans += (int) Math.pow(2, m - 1);
-        }
+            int matches = (int) Arrays.stream(nums[1].trim().split(" +")).filter(set::contains).count();
+            return (int) Math.pow(2, matches - 1);
+        }).sum();
         print("%d", ans);
     }
 }
