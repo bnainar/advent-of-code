6e7cc22ecd46f488daba7fd857d43b18177363f4
Mohamed Nainar <nainar.mb@gmail.com>
2023-12-09 15:24:17 +0530

    aoc23d9 use List<int[]>

--- src/aoc23/Day9.java
+++ src/aoc23/Day9.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static utils.Utils.*;
 
@@ -13,13 +14,13 @@ public class Day9 {
         var lines = Files.readAllLines(path);
         int ans1 = 0, ans2 = 0;
         for (String line : lines) {
-            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-            var nums= getIntList(line);
+            var list = new ArrayList<int[]>();
+            var nums = getIntArray(line);
             list.add(nums);
-            while (nums.stream().anyMatch(n -> n != 0)) {
-                var newNums = new ArrayList<Integer>();
-                for (int j = 0; j + 1 < nums.size(); j++) {
-                    newNums.add(nums.get(j + 1) - nums.get(j));
+            while (Arrays.stream(nums).anyMatch(n -> n != 0)) {
+                var newNums = new int[nums.length - 1];
+                for (int j = 0; j + 1 < nums.length; j++) {
+                    newNums[j] = nums[j + 1] - nums[j];
                 }
                 list.add(nums = newNums);
             }
@@ -29,17 +30,17 @@ public class Day9 {
         print("Part 1: %d\nPart 2: %d", ans1, ans2);
     }
 
-    private static int part1(ArrayList<ArrayList<Integer>> list) {
+    private static int part1(ArrayList<int[]> list) {
         int prev = 0;
         for (int n = list.size() - 2; n >= 0; n--) {
-            prev += list.get(n).get(list.get(n).size() - 1);
+            prev += list.get(n)[list.get(n).length - 1];
         }
         return prev;
     }
-    private static int part2(ArrayList<ArrayList<Integer>> list) {
+    private static int part2(ArrayList<int[]> list) {
         int prev = 0;
         for (int n = list.size() - 2; n >= 0; n--) {
-            prev = list.get(n).get(0) - prev;
+            prev = list.get(n)[0] - prev;
         }
         return prev;
     }
