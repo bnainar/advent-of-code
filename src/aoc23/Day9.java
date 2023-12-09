f83c7875f1d1047a86414bdc868705b4b0b860d8
Mohamed Nainar <nainar.mb@gmail.com>
2023-12-09 19:46:57 +0530

    aoc23d9 reverse Intstream range

--- src/aoc23/Day9.java
+++ src/aoc23/Day9.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
-import static utils.Utils.*;
+import static utils.Utils.getIntArray;
+import static utils.Utils.print;
 
 public class Day9 {
     public static void main(String[] args) throws IOException {
@@ -15,33 +16,21 @@ public class Day9 {
         int ans1 = 0, ans2 = 0;
         for (String line : lines) {
             var list = new ArrayList<int[]>();
-            var nums = getIntArray(line);
+            int[] nums = getIntArray(line);
             list.add(nums);
-            while (Arrays.stream(nums).anyMatch(n -> n != 0)) {
-                var newNums = new int[nums.length - 1];
-                for (int j = 0; j + 1 < nums.length; j++) {
-                    newNums[j] = nums[j + 1] - nums[j];
-                }
-                list.add(nums = newNums);
+            while (IntStream.of(nums).anyMatch(n -> n != 0)) {
+                var diff = new int[nums.length - 1];
+                for (int i = 0; i + 1 < nums.length; i++)
+                    diff[i] = nums[i + 1] - nums[i];
+
+                list.add(nums = diff);
             }
-            ans1 += part1(list);
-            ans2 += part2(list);
+
+            ans1 += list.stream().mapToInt(a -> a[a.length - 1])
+                    .sum();
+            ans2 += IntStream.range(2 - list.size(), 1)
+                    .reduce(0, (prev, n) -> list.get(-n)[0] - prev);
         }
         print("Part 1: %d\nPart 2: %d", ans1, ans2);
-    }
-
-    private static int part1(ArrayList<int[]> list) {
-        int prev = 0;
-        for (int n = list.size() - 2; n >= 0; n--) {
-            prev += list.get(n)[list.get(n).length - 1];
-        }
-        return prev;
-    }
-    private static int part2(ArrayList<int[]> list) {
-        int prev = 0;
-        for (int n = list.size() - 2; n >= 0; n--) {
-            prev = list.get(n)[0] - prev;
-        }
-        return prev;
     }
 }
