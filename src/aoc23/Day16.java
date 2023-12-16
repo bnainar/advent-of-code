674731de25a08c6cd37198d0037e9e6b304576ee
Mohamed Nainar <nainar.mb@gmail.com>
2023-12-16 13:52:56 +0530

    aoc23d16

---
+++ src/aoc23/Day16.java
@@ -0,0 +1,107 @@
+package aoc23;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+import static utils.Utils.print;
+
+public class Day16 {
+    enum Dir {LEFT, RIGHT, UP, DOWN}
+
+    static final Map<Dir, int[]> map = Map.of(
+            Dir.LEFT, new int[]{0, -1},
+            Dir.RIGHT, new int[]{0, 1},
+            Dir.UP, new int[]{-1, 0},
+            Dir.DOWN, new int[]{1, 0});
+
+    record Beam(int x, int y, Dir dir) { }
+    static int R = 0;
+    static int C = 0;
+    public static void main(String[] args) throws IOException {
+        String path = "C:/dev/advent-of-code/inputs/t";
+        var input = (ArrayList<String>) Files.readAllLines(Paths.get(path));
+        R = input.size();
+        C = input.get(0).length();
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < R; i++) {
+            max = Math.max(max, f(new Beam(i, 0, Dir.RIGHT), input));
+            max = Math.max(max, f(new Beam(i, C - 1, Dir.LEFT), input));
+        }
+        for (int j = 0; j < C; j++) {
+            max = Math.max(max, f(new Beam(0, j, Dir.DOWN), input));
+            max = Math.max(max, f(new Beam(R - 1, j, Dir.UP), input));
+        }
+        print("Part 1: %d", f(new Beam(0, 0, Dir.RIGHT), input));
+        print("Part 2: %d", max);
+    }
+
+    private static int f(Beam seed, ArrayList<String> input) {
+        var vis = new boolean[R][C];
+        var q = new ArrayDeque<Beam>();
+        var set = new HashSet<Beam>();
+        q.offer(seed);
+        while (!q.isEmpty()) {
+            var curr = q.poll();
+            int x = curr.x, y = curr.y;
+            if (!set.add(curr)) continue;
+            vis[x][y] = true;
+            char tile = input.get(x).charAt(y);
+            switch (tile) {
+                case '.':
+                    redirectBeam(q, x, y, curr.dir);
+                    break;
+                case '|':
+                    switch (curr.dir) {
+                        case UP:
+                        case DOWN:
+                            redirectBeam(q, x, y, curr.dir);
+                            break;
+                        default:
+                            redirectBeam(q, x, y, Dir.UP);
+                            redirectBeam(q, x, y, Dir.DOWN);
+                    }
+                    break;
+                case '-':
+                    switch (curr.dir) {
+                        case LEFT:
+                        case RIGHT:
+                            redirectBeam(q, x, y, curr.dir);
+                            break;
+                        default:
+                            redirectBeam(q, x, y, Dir.LEFT);
+                            redirectBeam(q, x, y, Dir.RIGHT);
+                    }
+                    break;
+                default:
+                    switch (curr.dir) {
+                        case LEFT:
+                            redirectBeam(q, x, y, tile == '/' ? Dir.DOWN : Dir.UP);
+                            break;
+                        case RIGHT:
+                            redirectBeam(q, x, y, tile == '/' ? Dir.UP : Dir.DOWN);
+                            break;
+                        case UP:
+                            redirectBeam(q, x, y, tile == '/' ? Dir.RIGHT : Dir.LEFT);
+                            break;
+                        case DOWN:
+                            redirectBeam(q, x, y, tile == '/' ? Dir.LEFT : Dir.RIGHT);
+                    }
+            }
+        }
+        int ans = 0;
+        for (var row : vis)
+            for (var t : row)
+                if (t) ans++;
+        return ans;
+    }
+
+    private static void redirectBeam(ArrayDeque<Beam> q, int x, int y, Dir d) {
+        var delta = map.get(d);
+        int nx = x + delta[0], ny = y + delta[1];
+        if (nx < 0 || nx >= R || ny < 0 || ny >= C) return;
+        q.offer(new Beam(nx, ny, d));
+    }
+
+}
---
+++ src/aockt/Day15.kt
@@ -0,0 +1,51 @@
+package aockt
+
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.*
+
+object Day15 {
+    @Throws(IOException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val path = "C:/dev/advent-of-code/inputs/t"
+        val lines = Files.readString(Paths.get(path)).split(",".toRegex()).toTypedArray()
+        part1(lines)
+        part2(lines)
+    }
+    private fun getHash(l: String): Int {
+        return l.chars().reduce(0) { acc: Int, c: Int -> (acc + c) * 17 % 256 }
+    }
+
+    private fun part1(lines: Array<String>) {
+        println("Part 1: " + lines.sumOf { getHash(it) })
+    }
+
+    private fun part2(lines: Array<String>) {
+        val boxes = Array(256) { mutableListOf<Lens>() }
+        for (line in lines) {
+            var op = line.indexOf('-')
+            if (op == -1) op = line.indexOf('=')
+            val label = line.substring(0, op)
+            val h = getHash(label)
+            if (line[op] == '-') {
+                boxes[h].removeIf { it.label == label }
+            } else {
+                val newLens = Lens(label, line[op + 1].code - '0'.code)
+                val idx = boxes[h].indexOfFirst { it.label == label }
+                if (idx == -1)
+                    boxes[h].add(newLens)
+                else
+                    boxes[h][idx] = newLens
+            }
+        }
+        var ans = 0
+        boxes.forEachIndexed { i, x ->
+            x.forEachIndexed { j, y ->
+                ans += (i + 1) * (j + 1) * y.focalLength } }
+        println("Part 2: $ans")
+    }
+
+    internal data class Lens(val label: String, val focalLength: Int)
+}
